07fed5afc74aecdafffb401eb83cc6cefe783073
mazir210 <mazir210@gmail.com>
2021-02-18 00:53:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ var cors = require('cors');
 app.use(
     cors({
         credentials: true,
-        origin: 'https://www.michaelboeynaems.com'
+        origin: 'https://app-monir2021.herokuapp.com/'
     })
 );
 app.options('*', cors());
 
-app.get('/', (req, res) => res.send('This api works.cors'));
+app.get('/', (req, res) => res.send('This api works.cors ,origin changed'));
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('server running on port 3000', '');
