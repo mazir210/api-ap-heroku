cf28ba3cc553e19788aa19fd399337e55d3a41fc
mazir210 <mazir210@gmail.com>
2021-02-18 01:17:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 
-app.use(
+app.use(cors())
+/*app.use(
     cors({
         credentials: true,
         origin: 'https://12sop.netlify.app'
     })
-);
+);*/
 app.options('*', cors());
 
 app.get('/', (req, res) => res.send('This api works.cors ,origin changed her'));
