50695be7ae8823615bd1053fc43e030aec9c814e
mazir210 <mazir210@gmail.com>
2021-02-18 01:24:02 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var cors = require('cors');
 
-app.use(cors())
+//app.use(cors())
 /*app.use(
     cors({
         credentials: true,
@@ -11,7 +11,7 @@ app.use(cors())
 );*/
 app.options('*', cors());
 
-app.get('/', (req, res) => res.send('This api works.cors ,origin changed wih app.use(cors())'));
+app.get('/', (req, res) => res.send('This api works.cors ,origin changed wihout app.use(cors())'));
 
 app.listen(process.env.PORT || 3000, function() {
     console.log('server running on port 3000', '');
